292e6446a21018866bad004002cc495af7b3d72f
Surya0144 <134142309+Surya0144@users.noreply.github.com>
2024-08-22 10:57:20 +0530

    Update week2_b.asm

--- week2_b.asm
+++ week2_b.asm
@@ -1,5 +1,52 @@
 org 100h
-mov ax,21A3h
-mov bx,9453h
-add ax,bx
+
+num1 dw 1A2Bh
+num2 dw 4C9Fh 
+start: mov ax,num1
+       add ax,num2
+mov bx,ax  
+;converting higher nibble
+mov ax,bx
+and ax,0F000h
+shr ax,12
+add ax,30h
+cmp ax,39h
+jle first_bit
+add ax,7h
+first_bit:mov dx,ax
+          mov ah,02h
+          int 21h
+;converting lower nibble
+mov ax,bx
+and ax,0F00h
+shr ax,8
+add ax,30h
+cmp ax,39h
+jle second_bit
+add ax,7h
+second_bit:mov dx,ax
+           mov ah,02h
+           int 21h 
+;3rd
+mov ax,bx
+and ax,00F0h
+shr ax,4
+add ax,30h
+cmp ax,39h
+jle third_bit
+add ax,7h
+third_bit:mov dx,ax
+           mov ah,02h
+           int 21h 
+mov ax,bx
+and ax,000Fh
+add ax,30h
+cmp ax,39h
+jle last_bit
+add ax,7h
+last_bit:mov dx,ax
+           mov ah,02h
+           int 21h                       
 ret
+
+ 
